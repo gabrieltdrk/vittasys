2578a51903cf189d261ff05301d6f33753a41ba8
Gabriel <gabriel.andradesx@gmail.com>
2025-08-21 16:57:15 -0300

    feat: primeiro commit + sidebar menu adicionado

---
+++ src/app/atendimento/page.tsx
@@ -0,0 +1,5 @@
+export default function Atendimento() {
+  return (
+    <h1>Aqui é o atendimento</h1>
+  );
+}
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,80 @@
+import { Calendar, Inbox, Search, Settings, UserCheck } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { NavUser } from "./nav-user"
+
+// Menu items.
+const items = [
+  {
+    title: "Atendimento",
+    url: "./atendimento",
+    icon: UserCheck,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  }
+}
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarGroupLabel>VittaSYS</SidebarGroupLabel>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
